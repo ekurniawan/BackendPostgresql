76a528eb78616d86bfa0f42b589f452632f0032e
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-02-06 11:55:45 +0700

    menambahkan method untuk insert data

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using DAL;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestaurantDAL resto = new RestaurantDAL();
+            var results = resto.GetAll();
+            foreach(var data in results){
+                Console.WriteLine(data.restaurantid + " " + data.namarestaurant);
+            }
+        }
+    }
+
+}
--- DAL/RestaurantDAL.cs
+++ DAL/RestaurantDAL.cs
@@ -16,13 +16,12 @@ namespace DAL
         //get data
         public IEnumerable<Restaurant> GetAll()
         {
-
             using (NpgsqlConnection conn = new NpgsqlConnection(GetConnStr()))
             {
                 List<Restaurant> lstResto = new List<Restaurant>();
                 var strSql = @"select * from restaurants order by namarestaurant";
                 NpgsqlCommand cmd = new NpgsqlCommand(strSql, conn);
-
+                conn.Open();
                 NpgsqlDataReader dr = cmd.ExecuteReader();
                 if (dr.HasRows)
                 {
@@ -41,6 +40,33 @@ namespace DAL
                 conn.Close();
 
                 return lstResto;
+            }
+        }
+
+        public void InsertData(Restaurant resto)
+        {
+            using(NpgsqlConnection conn = new NpgsqlConnection(GetConnStr()))
+            {
+                var strSql = @"insert into restaurants(namarestaurant) 
+                               values(@namarestaurant)";
+
+                NpgsqlCommand cmd = new NpgsqlCommand(strSql, conn);
+                cmd.Parameters.AddWithValue("@namarestaurant", 
+                    resto.namarestaurant);
+                try
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                catch (NpgsqlException sqlEx)
+                {
+                    throw new Exception("Error: "+sqlEx.Message);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
             }
         }
     }
