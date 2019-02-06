6af0343e0b83e9c67a50870a9b5b63343ed9e8af
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-02-06 13:07:56 +0700

    menambahkan insert method

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -13,6 +13,21 @@ namespace ConsoleClient
         static void Main(string[] args)
         {
             RestaurantDAL resto = new RestaurantDAL();
+
+            try
+            {
+                var newData = new Restaurant
+                {
+                    namarestaurant = "Filosofi Kopi"
+                };
+                resto.InsertData(newData);
+                Console.WriteLine("Data berhasil ditambahkan");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Insert data gagal {ex.Message}");
+            }
+
             var results = resto.GetAll();
             foreach(var data in results){
                 Console.WriteLine(data.restaurantid + " " + data.namarestaurant);
